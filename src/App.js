8cc0454962999d9eeacdab6285fef11e54244269
Hugo Noppe <hugonoppe444@gmail.com>
2025-01-28 16:03:51 +0100

    Mise en place du projet entier, react et tailwind

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white min-h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold">Bienvenue dans mon application Ergogo !</h1>
+    </div>
+  );
+}
+
+export default App;
+
